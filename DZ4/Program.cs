4bd288d41db71d63fba00b3f062e8cf5512dd73b
Andrew12051996 <96420153+Andrew12051996@users.noreply.github.com>
2023-04-13 11:47:59 +0300

    залил дз по первому семинару

---
+++ DZ4/Program.cs
@@ -0,0 +1,12 @@
+﻿// Вывод четных элементов
+Console.WriteLine("Enter number: ");
+string FirstNum = Console.ReadLine();
+int Num = Convert.ToInt32(FirstNum);
+for (int i = 1; i <= Num; i++)
+    {
+        if (i % 2 == 0) 
+        {
+        Console.WriteLine(i);
+        }
+        
+    }
