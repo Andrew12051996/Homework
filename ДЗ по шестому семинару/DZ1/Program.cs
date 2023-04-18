34166780c42663aec2000d8c504724746bdde9ff
Andrew12051996 <96420153+Andrew12051996@users.noreply.github.com>
2023-04-18 18:40:02 +0300

    Шестой семинар

---
+++ ДЗ по шестому семинару/DZ1/Program.cs
@@ -0,0 +1,55 @@
+﻿// Сколько чисел больше 0 ввёл пользователь.
+
+
+/*int[] FillArray (int length, int minValue, int maxValue)
+{
+    int[] result = new int[length];
+    Random rand = new Random();
+    for(int i = 0; i < length; i++)
+    {
+        result[i] = rand.Next(minValue, maxValue);
+    }
+    return result;
+}
+int GetSum (int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            sum = sum + 1;
+        }
+    }
+    return sum;
+}
+int[] array = FillArray (100, -100, 100);
+Console.WriteLine(string.Join(", ", array));
+Console.WriteLine(string.Join(", ", GetSum(array)));*/
+int[] FillArray (int length)
+{
+    int[] result = new int[length];
+    Console.WriteLine("Введите число элементов массива ");
+    length = int.Parse(Console.ReadLine());
+    for(int i = 0; i < length; i++)
+        {
+        Console.WriteLine($"Введите {i+1} число массива ");
+        result[i] = int.Parse(Console.ReadLine());
+        }
+    return result;
+}
+int GetSum (int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            sum = sum + 1;
+        }
+    }
+    return sum;
+}
+int[] array = FillArray (10);
+Console.WriteLine(string.Join(", ", array));
+Console.WriteLine(string.Join(", ", GetSum(array)));
---
+++ ДЗ по шестому семинару/DZ2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Точка пересечения.
+Console.WriteLine("Введите координату k1 первой точки ");
+double k1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите координату b1 первой точки ");
+double b1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите координату k2 второй точки ");
+double k2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите координату b2 первой точки ");
+double b2 = int.Parse(Console.ReadLine());
+double X = (b2 - b1) / (k2 - k1);
+double Y = (k1 * X + b1) - (k2 * X - b2);
+if((k1 * X + b1) == (k2 * X + b2))
+{
+    Console.WriteLine("Прямые параллельны");
+}
+else {
+   
+    Console.WriteLine($"Точка пеересечения {X}, {Y}");
+}
